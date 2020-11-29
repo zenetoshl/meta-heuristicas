7c3615e6d8e39b856b80634aa02b42236ee858cb
Jose Neto <zenetoshl@gmail.com>
2020-11-29 07:36:55 -0300

    oia, eu acho que ta pronto

--- algoritmo-genetico/genetic1.py
+++ algoritmo-genetico/genetic1.py
@@ -1,56 +1,146 @@
 import math
 import random as rd
+import pandas as pd
 
-#Constants
+#Constantes
 P = 2
 sig = 2
 L = 100
+N = 1000
+I = 500
+best = 100000
+oldBest = 10000000
+globalBest = 10000000
+gen = 0
+maxGen = 1000
+bestX = [0, 0]
 
-def g1 (x1, x2):
-    return ((((math.sqrt(2) * x1 + x2)/(math.sqrt(2)*(x1**2) + (2*x1*x2))) * P) - sig)
+#obj solution, x é o vetor de decisão e fitness é o fitness calculado para o vetor
+class solution:
+    def __init__(self, x):
+        self.x = x
+        self.mutate()
+        self.check_lim()
+        self.fitness = self.fit(x)
+        
+    def fit(self, x):
+        x1 = x[0]
+        x2 = x[1]
+        fitness = self.f() * self.check_restritions(x)
+        return fitness
 
-def g2 (x1, x2):
-    return ((((x2)/(math.sqrt(2)*(x1**2) + (2*x1*x2))) * P) - sig)
+    #checando as restrições g1, g2, e g3, cada uma delas adiciona um peso no fitness final caso não seja satisfeita
+    def check_restritions(self, x):
+        x1 = x[0]
+        x2 = x[1]
+        multiplier = 0
+        if(not (self.g1() <= 0 )):
+            multiplier = multiplier + 1000000
+        if(not (self.g2() <= 0) ):
+            multiplier = multiplier + 1000000
+        if(not (self.g3() <= 0) ):
+            multiplier = multiplier + 1000000
+        if(multiplier == 0):
+            multiplier = 1
+        return multiplier
 
-def g3 (x1, x2):
-    return ((((1)/(math.sqrt(2)*(x2) + x1)) * P) - sig)
+    def mutate(self):
+        if(rd.uniform(0, 1) > 0.98):
+            for i in range(2):
+                u = rd.uniform(0, 1)
+                pert = 0.1 * ((2*u) - 1)
+                self.x[i] = self.x[i] + pert
+    
+    def check_lim(self):
+        for i in range(2):
+            if(self.x[i] <= 0):
+                self.x[i] = 0.00001
+            elif(self.x[i] > 1):
+                self.x[i] = 1
 
-def f(x1, x2):
-    return ((2 * (math.sqrt(2) * x1) + x2) * L)
+    def g1 (self):
+        return ((((math.sqrt(2) * self.x[0] + self.x[1])/(math.sqrt(2)*(self.x[0]**2) + (2*self.x[0]*self.x[1]))) * P) - sig)
 
-class Complex:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
+    def g2 (self):
+        return ((((self.x[1])/(math.sqrt(2)*(self.x[0]**2) + (2*self.x[0]*self.x[1]))) * P) - sig)
+
+    def g3 (self):
+        return ((((1)/(math.sqrt(2)*(self.x[1]) + self.x[0])) * P) - sig)
+
+    def f(self):
+        return ((2 * (math.sqrt(2) * self.x[0]) + self.x[1]) * L)
 
 
-def init():
+#Gera umindividuo aleatório de acordo com os limites do problema
+def generate(lim):
     l = []
     for i in range (2):
-        x = rd.uniform(0, 1)
+        x = rd.uniform(lim[0], lim[1])
         l.append(x)
     return l
 
-def check_restritions(x):
-    x1 = x[0]
-    x2 = x[1]
-    return (g1(x1, x2) <= 0 and g2(x1, x2) <= 0  and g3(x1, x2) <= 0)
-
-def fit(x):
-    x1 = x[0]
-    x2 = x[1]
-    fitness = f(x1, x2)
-    if(not check_restritions(x)):
-        fitness = fitness * 1.5
-    return fitness
-
-
+#Gera uma população de tamanho n aleatoriamente de acordo com o limites do problema
 def generate_population(n):
     r = []
     for i in range(n):
-        r.append(init())
+        r.append(solution(generate([0,1])))
     return r
 
-x = generate_population(1000)
-for item in x:
-    print(fit(item))
+#Cruzamento para uma nova geração
+def breed_generation(pop):
+    global gen
+    gen = gen + 1
+    breeded_generation = []
+    for i in range(N):
+        p = []
+        p.append(select(pop))
+        p.append(select(pop))
+        if(p[0].fitness > p[1].fitness):
+            breeded_generation.append(breed(p[1],p[0]))
+        else:
+            breeded_generation.append(breed(p[0],p[1]))
+        p.clear()
+    return breeded_generation
+
+#selecao por torneio com n=3
+def select(pop):
+    selection = []
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.sort(key=lambda x: x.fitness, reverse=False)
+    return selection[0]
+
+#Cruzamento linear
+def breed(parent1, parent2):
+    global best, oldBest, globalBest
+    child = solution([parent1.x[0], parent2.x[1]])
+    if(child.fitness < best):
+        oldBest = best
+        best = child.fitness
+        if(best < globalBest):
+            globalBest = best
+            bestX = child.x
+    return child
+
+best = 100000
+oldBest = 1000000
+pop = generate_population(N)
+best_genetic_list  = []
+for i in range(I):
+    best = 100000
+    oldBest = 1000000
+    gen = 0
+    pop = generate_population(N)
+    while ((oldBest - best) > 0.1 and maxGen > gen):
+        pop = breed_generation(pop)
+    best_genetic_list.append(best)
+    print(best)
+min  = solution([0.7886751284,  0.4082483080])
+print(min.fitness)
+print()
+print(bestX)
+
+best_genetic_series = pd.Series(best_genetic_list)
+
+print(best_genetic_series.describe())
--- algoritmo-genetico/genetic2.py
+++ algoritmo-genetico/genetic2.py
@@ -1,27 +1,87 @@
 import math
 import random as rd
+import pandas as pd
 
-#Constants
+#Constantes
 P = 2
 sig = 2
 L = 100
+N = 25000
+I = 100
+best = 10000000
+bestx = [0, 0, 0, 0]
+oldBest = 1000000000
+gen = 0
+maxGen = 1000
 
-def g1 (x1, x2, x3, x4):
-    return (-x1 + 0.0193*x3)
+class solution:
+    def __init__(self, x):
+        self.x = x
+        self.mutate()
+        self.check_lim()
+        self.fitness = self.fit(x)
+        
+    def fit(self, x):
+        fitness = self.f() * self.check_restritions(x)
+        return fitness
 
-def g2 (x1, x2, x3, x4):
-    return (-x2 + 0.00954*x3)
+    #checando as restrições g1, g2, e g3, cada uma delas adiciona um peso no fitness final caso não seja satisfeita
+    def check_restritions(self, x):
+        multiplier = 1
+        if(not (self.g1() <= 0 )):
+            print('g1')
+            multiplier = multiplier + 10000000000
+        if(not (self.g2() <= 0) ):
+            print('g2')
+            multiplier = multiplier + 10000000000
+        if(not (self.g3() <= 0) ):
+            print('g3')
+            multiplier = multiplier + 10000000000
+        if(not (self.g4() <= 0) ):
+            print('g4')
+            multiplier = multiplier + 10000000000
+        return multiplier
 
-def g3 (x1, x2, x3, x4):
-    return ((-(math.pi*(x3**2)*x4) - (4/3)*math.pi*(x3**3)) + 1296000)
+    def mutate(self):
+            for i in range(4):
+                if(rd.uniform(0, 1) > 1):
+                    u = rd.uniform(0, 1)
+                    if(i < 2):
+                        pert = 0.2 * (100) * ((2*u) - 1)
+                    else:
+                        pert = 0.2 * (190) * ((2*u) - 1)
+                    self.x[i] = self.x[i] + pert
     
-def g4 (x1, x2, x3, x4):
-    return (x4 - 240)
+    def check_lim(self):
+        for i in range(4):
+            if(i < 2):
+                if(self.x[i] <= 0):
+                    self.x[i] = 0.00000001
+                elif(self.x[i] > 100):
+                    self.x[i] = 100
+            else:
+                if(self.x[i] <= 10):
+                    self.x[i] = 10
+                elif(self.x[i] > 200):
+                    self.x[i] = 200
+    
+    def g1 (self):
+        return (-self.x[0] + 0.0193*self.x[2])
 
-def f(x1, x2, x3, x4):
-    return (0.6224*x1*x3*x4 + 1.7781*x2*(x3**2) + 3.1661*(x1**2)*x4+19.84*(x1**2)*x3)
+    def g2 (self):
+        return (-self.x[1] + 0.00954*self.x[2])
 
-def init():
+    def g3 (self):
+        return ((-(math.pi*(self.x[2]**2)*self.x[3]) - (4/3)*math.pi*(self.x[2]**3)) + 1296000)
+
+    def g4 (self):
+        return (self.x[3] - 240)
+
+    def f(self):
+        return (0.6224*self.x[0]*self.x[2]*self.x[3] + 1.7781*self.x[1]*(self.x[2]**2) + 3.1661*(self.x[0]**2)*self.x[3]+19.84*(self.x[0]**2)*self.x[2])
+
+
+def generate():
     l = []
     x1 = rd.uniform(0, 100)
     l.append(x1)
@@ -33,23 +93,94 @@ def init():
     l.append(x4)
     return l
 
-def check_restritions(x):
-    x1 = x[0]
-    x2 = x[1]
-    x3 = x[2]
-    x4 = x[3]
-    print("g1")
-    print(g1(x1, x2, x3, x4))
-    print("g2")
-    print(g2(x1, x2, x3, x4))
-    print("g3")
-    print(g3(x1, x2, x3, x4))
-    print("g4")
-    print(g4(x1, x2, x3, x4))
-    print('f')
-    print(f(x1, x2, x3, x4))
-    return (g1(x1, x2, x3, x4) <= 0 and g2(x1, x2, x3, x4) <= 0  and g3(x1, x2, x3, x4) <= 0 and g4(x1, x2, x3, x4) <= 0)
+#Gera uma população de tamanho n aleatoriamente de acordo com o limites do problema
+def generate_population(n):
+    r = []
+    for i in range(n):
+        r.append(solution(generate()))
+    return r
 
-print(check_restritions([0.812500,  0.437500,  42.09844539, 176.63659855]))
-x = init()
-print(x)
+#Cruzamento para uma nova geração
+def breed_generation(pop):
+    global gen
+    gen = gen + 1
+    pop.sort(key=lambda x: x.fitness, reverse=False)
+    pop = pop [0:int(N/2)]
+    breeded_generation = [pop[0]]
+    for i in range(N - 1):
+        p = []
+        p.append(select(pop))
+        p.append(select(pop))
+        breeded_generation.append(breed(p[0],p[1]))
+        p.clear()
+    return breeded_generation
+
+#selecao por torneio com n=3
+def select(pop):
+    selection = []
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.append(rd.choice(pop))
+    selection.sort(key=lambda x: x.fitness, reverse=False)
+    return selection[0]
+
+#Cruzamento linear
+def breed(parent1, parent2):
+    global best, oldBest, bestx
+    child = solution(set_genes(parent1, parent2))
+    if(child.fitness < best):
+        oldBest = best
+        best = child.fitness
+        bestx  = child.x
+    return child
+
+def set_genes(p1, p2):
+    np1 = 0
+    np2 = 0
+    genes = []
+    for i in range(4):
+        if(rd.uniform(0, 1) >= 0.5):
+            np1 = np1 + 1
+            if (np1 >= 2):
+                genes.append(p2.x[i])
+                np2 = np2 + 1
+            else:
+                genes.append(p1.x[i])            
+        else:
+            np2 = np2 + 1
+            if (np2 >= 2):
+                genes.append(p1.x[i])
+                np1 = np1 + 1
+            else:
+                genes.append(p2.x[i])
+    return genes
+
+
+optimal = solution([0.8419575940040076, 0.415492898727734, 43.42255673512729, 161.0265340906853])
+print(optimal.fitness)
+
+best = 10000000
+bestx = [0, 0, 0, 0]
+oldBest = 1000000000
+best_genetic_list  = []
+for i in range(I):
+    best = 10000000
+    bestx = [0, 0, 0, 0]
+    oldBest = 1000000000
+    gen = 0
+    pop = generate_population(N)
+    while ((oldBest - best) > 0.4 and maxGen > gen):
+        pop = breed_generation(pop)
+        if(optimal.fitness > best):
+            print(print(bestx))
+            break
+    best_genetic_list.append(best)
+    print(bestx)
+    print(best)
+
+best_genetic_series = pd.Series(best_genetic_list)
+
+print(best_genetic_series.describe())
